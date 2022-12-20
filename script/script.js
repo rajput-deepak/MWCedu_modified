fd72fb0955d95fc4f44fab52f2a6150d4d09d79a
rajput-deepak <deepak.rajputm24@gmail.com>
2022-12-20 14:28:30 +0530

    form done

--- script/script.js
+++ script/script.js
@@ -23,10 +23,28 @@ category_toggle.onclick = function(){
 
 let form_close = document.getElementById("form_close");
 let form_sec = document.getElementById("form_sec");
-let form_signin = document.getElementById("form_sec");
-let form_signup = document.getElementById("form_sec");
+let login_btn = document.getElementById("login_btn");
+let login = document.getElementById("login");
 
+login.onclick = function(){
+  form_sec.style.display = "flex";
+}
 
+login_btn.onclick = function(){
+  form_sec.style.display = "flex";
+}
+form_close.onclick = function(){
+  form_sec.style.display = "none";
+}
 
+document.getElementById("signin_here").onclick = function(){
+  document.getElementById("form_signup_cont").style.display = "none";
+  document.getElementById("form_signin_cont").style.display = "flex";
+}
+
+document.getElementById("signup_here").onclick = function(){
+  document.getElementById("form_signup_cont").style.display = "flex";
+  document.getElementById("form_signin_cont").style.display = "none";
+}
 
 

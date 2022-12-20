fc0d72223b2b01a601be389df3df7cbe176823e3
rajput-deepak <deepak.rajputm24@gmail.com>
2022-12-20 13:01:06 +0530

    working on forms

--- script/script.js
+++ script/script.js
@@ -9,3 +9,24 @@ nav_toggle.onclick = function(){
   // sidenav toggle
   sidenav.classList.toggle("active");
 }
+
+//============= category toggle
+
+let category_toggle = document.getElementById("category_toggle");
+let list = document.getElementById("drop_down");
+
+category_toggle.onclick = function(){
+  list.classList.toggle("active")
+}
+
+//=================== form
+
+let form_close = document.getElementById("form_close");
+let form_sec = document.getElementById("form_sec");
+let form_signin = document.getElementById("form_sec");
+let form_signup = document.getElementById("form_sec");
+
+
+
+
+
